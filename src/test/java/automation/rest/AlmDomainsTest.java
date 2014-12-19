6d24cef29ed648160d02bd01fd113207953dea1e
Sergey Belonozhko <sergey.belonozhko@hp.com>
2014-12-19 13:23:57 +0200

    domains+json

--- src/test/java/automation/rest/AlmDomainsTest.java
+++ src/test/java/automation/rest/AlmDomainsTest.java
@@ -6,11 +6,15 @@ import automation.rest.utils.CookieStorage;
 import org.apache.wink.client.ClientResponse;
 import org.apache.wink.client.Resource;
 import org.apache.wink.client.RestClient;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 /**
  * @author sergey.belonozhko@hp.com
@@ -34,6 +38,21 @@ public class AlmDomainsTest {
                 .accept(MediaType.APPLICATION_JSON_TYPE)
                 .get();
         Assert.assertEquals(200, domainsResponse.getStatusCode());
-        System.out.println(domainsResponse.getEntity(String.class));
+        String content = domainsResponse.getEntity(String.class);
+        System.out.println(content);
+        DomainsResult domainsResult = new ObjectMapper().readValue(content, DomainsResult.class);
+        System.out.println(domainsResult.totalCount);
+    }
+
+    public static class Domain {
+        public String name;
+        @JsonIgnore
+        public Object projects;
+    }
+
+    public static class DomainsResult {
+        public List<Domain> results;
+        @JsonProperty("total-count")
+        public int totalCount;
     }
 }
