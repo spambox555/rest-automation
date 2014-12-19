207c37493ba79645b096e2dfaa24a9b3f0d8d08b
Sergey Belonozhko <sergey.belonozhko@hp.com>
2014-12-19 13:14:29 +0200

    Simple serialization example

---
+++ src/test/java/automation/rest/serialization/SimpleSerializationJSON.java
@@ -0,0 +1,36 @@
+package automation.rest.serialization;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author sergey.belonozhko@hp.com
+ */
+public class SimpleSerializationJSON {
+    @Test
+    public void testSimpleObjectSerialization() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("simple-person.json");
+        Person person = new ObjectMapper().readValue(inputStream, Person.class);
+        System.out.println(person);
+        new ObjectMapper().writeValue(System.out, person);
+    }
+
+    public static class Person {
+        public String name;
+        public Integer age;
+        public List<String> socialNetworkProfiles;
+
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", socialNetworkProfiles=" + socialNetworkProfiles +
+                    '}';
+        }
+    }
+}
